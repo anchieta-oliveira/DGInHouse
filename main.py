cec975e74b428fda2a0c01ad694aa506791fe78d
Anchieta Oliveira <anchieta-13@hotmail.com>
2023-02-04 16:44:55 -0300

    Criação da entrada de flags

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
-from app.tools import read_arguments, read_data, calcule_Delta_G, calcule_probability, make_grafic_3D
+from app.tools import read_arguments, read_data, calcule_Delta_G, calcule_probability, make_grafic_3D, save_data
+
+args = read_arguments()
 
 
+if '-traj' in args and '-cor' in args and '-top' in args:
+    # Criar data RG-RMSD
+    pass
 
-print(read_arguments())
+
+if '-data_apo' in args and '-data_holo' in args:
+    # Ler dados de RG-RMSD de arquivo
+    data = read_data(path_apo=args['-data_apo'], path_holo=args['-data_holo']).round(1)
 
 
-data = read_data()
+if '-data_DG' in args:
+    #Plota gráfico de dados salvos anteriormente
+    probability = calcule_probability(data=data)
+    delta_g = calcule_Delta_G(probability=probability, temp=args['-temp'], model = data['model'].tolist())
 
-p = calcule_probability(data=data.round(1))
 
-dg = calcule_Delta_G(probability=p, temp=310, model= data['model'].to_list())
+if '-show_grafic' in args or '-save_fig' in args:
+    probability = calcule_probability(data=data)
+    delta_g = calcule_Delta_G(probability=probability, temp=args['-temp'], model = data['model'].tolist())
+    if '-show_grafic' in args:
+        make_grafic_3D(data=data, data_dg=delta_g, save_fig=False, show_grafic=args['-show_grafic'])
 
-make_grafic_3D(data=data, path='./', data_dg=dg, save_fig=False, show_grafic=True)
+    if '-save_fig' in args:
+        make_grafic_3D(data=data, path=args['-path'], data_dg=delta_g, save_fig=args['-save_fig'], show_grafic=False)
+
+
+if '-save_data' in args:
+    # Salva valores de RG-RMSD-DG em arquivo
+    save_data(data_rmsd=data['RMSD'].tolist(), data_rg=data['RG'].tolist(), data_dg=delta_g, path=args['-path'])
+
