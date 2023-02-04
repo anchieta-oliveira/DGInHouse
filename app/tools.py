992648ac73ccac2c88dd8fc3a2e580080084677e
Anchieta Oliveira <anchieta-13@hotmail.com>
2023-02-04 16:48:41 -0300

    Criação da função make_grafic_2D

--- app/tools.py
+++ app/tools.py
@@ -137,3 +137,6 @@ def make_grafic_3D(data: pd,  data_dg: list, save_fig: bool, show_grafic: bool, 
 
     if show_grafic:
         plt.show()
+
+def make_grafic_2D(data: pd,  data_dg: list, save_fig: bool, show_grafic: bool, path: str = "./"):
+    pass 
