1591b1b78b65c08aa533c9c2301af3f93474160d
Anchieta Oliveira <anchieta-13@hotmail.com>
2023-02-06 14:55:41 -0300

    Criação das funções que escrevem os scripts para o vmd

--- app/rmsd_rg_vmd.py
+++ app/rmsd_rg_vmd.py
@@ -1,83 +1,85 @@
-SCRIPT_RG_RMSD= """
-package require pbctools
+
+def script_rg_rmsd(cord: str, top: str, traj: str, prefix_out:str, path: str, selection:str) -> str:
+    script_rg_rmsd = """
+    package require pbctools 
+
+    mol load """ + cord.split(".")[-1] + " " + cord + " " + top.split(".")[-1] + " " + top + """ 
+    mol addfile """ +  traj + """ type """ +  traj.split(".")[-1] + """waitfor all
+
+    pbc unwrap -sel  '""" + selection + """'
+
+    set outfile [open""" +  path + "/" + prefix_out + """.dat w];
+    set nf [molinfo top get numframes]
+    set sel_protein_cg [atomselect top '""" + selection + """']
+    set frame0 [atomselect top '""" + selection + """' frame 0]
+    puts $outfile "RG\tRMSD"
 
 
-mol load gro ../3oty_cg_ion.gro psf ../3oty_cg_ion.psf 
-mol addfile 3oty_cg_md.xtc type xtc waitfor all
+    proc gyr_radius {sel} {
+    # make sure this is a proper selection and has atoms
+    if {[$sel num] <= 0} {
+        error "gyr_radius: must have at least one atom in selection"
+    }
+    # gyration is sqrt( sum((r(i) - r(center_of_mass))^2) / N)
+    set com [measure center $sel weight mass]
+    set sum 0
+    foreach coord [$sel get {x y z}] {
+        set sum [vecadd $sum [veclength2 [vecsub $coord $com]]]
+    }
+    return [expr sqrt($sum / ([$sel num] + 0.0))]
+    }
 
 
-pbc unwrap -sel "resid 1 to 99"
+    # rg and RMSD calculation loop
+    for {set i 1 } {$i < $nf } { incr i } {
+        $sel_protein_cg frame $i
+        $sel_protein_cg move [measure fit $sel_protein_cg $frame0]
+        puts $outfile "[gyr_radius $sel_protein_cg]\t[measure rmsd $sel_protein_cg $frame0]"
+    }
+    close $outfile
+    quit """
+
+    return script_rg_rmsd
 
 
-set outfile [open rg_cg_and_rmsd.dat w];
-set nf [molinfo top get numframes]
-set sel_protein_cg [atomselect top "resid 1 to 99"]
-set frame0 [atomselect top "resid 1 to 99" frame 0]
-puts $outfile "RG\tRMSD"
+def script_rg_rmsd_big_traj(cord: str, top: str, traj: str, prefix_out:str, path: str, selection:str) -> str:
+    script_rg_rmsd_big_traj = """
+    package require pbctools 
+    source bigdcd.tcl
 
+    mol load """ + cord.split(".")[-1] + " " + cord + " " + top.split(".")[-1] + " " + top + """  
 
-proc gyr_radius {sel} {
-  # make sure this is a proper selection and has atoms
-  if {[$sel num] <= 0} {
-    error "gyr_radius: must have at least one atom in selection"
-  }
-  # gyration is sqrt( sum((r(i) - r(center_of_mass))^2) / N)
-  set com [measure center $sel weight mass]
-  set sum 0
-  foreach coord [$sel get {x y z}] {
-    set sum [vecadd $sum [veclength2 [vecsub $coord $com]]]
-  }
-  return [expr sqrt($sum / ([$sel num] + 0.0))]
-}
+    set outfile [open""" +  path + "/" + prefix_out + """.dat w];
+    set sel_protein_cg [atomselect top '""" + selection + """']
+    set frame0 [atomselect top '""" + selection + """' frame 0]
+    puts $outfile "RG\tRMSD"
 
-
-# rg and RMSD calculation loop
-for {set i 1 } {$i < $nf } { incr i } {
-    $sel_protein_cg frame $i
+    proc rg_rmsd { frame } {
+    global frame0 sel_protein_cg outfile
+        proc gyr_radius {sel} {
+        # make sure this is a proper selection and has atoms
+        if {[$sel num] <= 0} {
+            error "gyr_radius: must have at least one atom in selection"
+        }
+        # gyration is sqrt( sum((r(i) - r(center_of_mass))^2) / N)
+        set com [measure center $sel weight mass]
+        set sum 0
+        foreach coord [$sel get {x y z}] {
+            set sum [vecadd $sum [veclength2 [vecsub $coord $com]]]
+        }
+        return [expr sqrt($sum / ([$sel num] + 0.0))]
+        }
+        
+    pbc unwrap -sel  '""" + selection + """'
     $sel_protein_cg move [measure fit $sel_protein_cg $frame0]
     puts $outfile "[gyr_radius $sel_protein_cg]\t[measure rmsd $sel_protein_cg $frame0]"
-}
-close $outfile
-quit """
+    }
 
-SCRIPT_RG_RMSD_BIG_TRAJ= """
-package require pbctools 
-source bigdcd.tcl
+    bigdcd rg_rmsd """ + traj + """
+    bigdcd_wait
 
-mol load gro ../3OUC_cg_ion.gro psf ../3OUC_cg_ion.psf 
+    close $outfile
+    quit
+    """
+    return script_rg_rmsd_big_traj
 
-set outfile [open rg_cg_and_rmsd_com_lig_teste.dat w];
-set sel_protein_cg [atomselect top "resid 1 to 99"]
-set frame0 [atomselect top "resid 1 to 99" frame 0]
-puts $outfile "RG\tRMSD"
-
-
-# rg and RMSD calculation loop
-
-proc rg_rmsd { frame } {
-  global frame0 sel_protein_cg outfile
-	  proc gyr_radius {sel} {
-	  # make sure this is a proper selection and has atoms
-	  if {[$sel num] <= 0} {
-	    error "gyr_radius: must have at least one atom in selection"
-	  }
-	  # gyration is sqrt( sum((r(i) - r(center_of_mass))^2) / N)
-	  set com [measure center $sel weight mass]
-	  set sum 0
-	  foreach coord [$sel get {x y z}] {
-	    set sum [vecadd $sum [veclength2 [vecsub $coord $com]]]
-	  }
-	  return [expr sqrt($sum / ([$sel num] + 0.0))]
-	}
-	
-   pbc unwrap -sel "resid 1 to 99"
-   $sel_protein_cg move [measure fit $sel_protein_cg $frame0]
-   puts $outfile "[gyr_radius $sel_protein_cg]\t[measure rmsd $sel_protein_cg $frame0]"
-}
-
-bigdcd rg_rmsd  3OUC_cg_md.xtc
-bigdcd_wait
-
-close $outfile
-quit
-"""
