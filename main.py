3c083cdf4c31605b33c891d78bd3d0c2d7c3895e
Anchieta Oliveira <anchieta-13@hotmail.com>
2023-02-06 19:54:22 -0300

    Adição na analise dos dados de trajetoria e etc

--- main.py
+++ main.py
@@ -1,27 +1,35 @@
-from app.tools import read_arguments, read_data, calcule_Delta_G, calcule_probability, make_grafic_3D, save_data
+from multiprocessing import Process
+from app.tools import read_arguments, read_data, calcule_Delta_G, calcule_probability, make_grafic_3D, save_data, calcule_rg_rmsd
+
 
 args = read_arguments()
 
 
-if '-traj' in args and '-cor' in args and '-top' in args:
+if '-traj_a' in args and '-cor_a' in args and '-top_a' in args:
     # Criar data RG-RMSD
-    pass
+    proc_a = Process(target=calcule_rg_rmsd, args=(args['-cor_a'], args['-top_a'], args['-traj_a'], args['-prefix_out'] + "_a", args['-path'], args['-sel'], args['-big_traj']))
+    proc_a.start()
+    args['-data_a']=args['-path'] + "/" + args['-prefix_out'] + "_a.dat"
 
 
-if '-data_a' in args and '-data_b' in args:
-    # Ler dados de RG-RMSD de arquivo
+if '-traj_b' in args and '-cor_b' in args and '-top_b' in args:
+    # Criar data RG-RMSD
+    proc_b = Process(target=calcule_rg_rmsd, args=(args['-cor_b'], args['-top_b'], args['-traj_b'], args['-prefix_out']+ "_b", args['-path'], args['-sel'], args['-big_traj']))
+    proc_b.start()
+    args['-data_b']=args['-path'] + "/" + args['-prefix_out'] + "_b.dat"
+
+
+if '-data_DG' in args or '-show_grafic' in args or '-save_fig' in args or '-save_data' in args:
+    if "-traj_a" in args or "-traj_b" in args:
+        proc_a.join()
+        proc_b.join()
+    
     data = read_data(path_a=args['-data_a'], path_b=args['-data_b']).round(1)
-
-
-if '-data_DG' in args:
-    #Plota gráfico de dados salvos anteriormente
     probability = calcule_probability(data=data)
     delta_g = calcule_Delta_G(probability=probability, temp=args['-temp'], model = data['model'].tolist())
 
 
 if '-show_grafic' in args or '-save_fig' in args:
-    probability = calcule_probability(data=data)
-    delta_g = calcule_Delta_G(probability=probability, temp=args['-temp'], model = data['model'].tolist())
     if '-show_grafic' in args:
         make_grafic_3D(data=data, data_dg=delta_g, save_fig=False, show_grafic=args['-show_grafic'])
 
